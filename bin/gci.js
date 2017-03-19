d95bb56b146204c0050d638801af652b3444c6f4
Pierre Charles <pierre.chls@gmail.com>
2017-03-19 19:07:30 +0100

    Initial commit

--- bin/gci.js
+++ bin/gci.js
@@ -1,16 +1,35 @@
 #!/usr/bin/env node
 
+var path = require('path')
 var program = require('commander')
-var path = require('path');
+var pkg = require(path.join('../package'))
 
 /**
  * Usage.
  */
 
 program
-  .version('1.0.0')
-  .usage('[options]')
+  .version(pkg.version)
+  .description('Simple CLI to easily commit multiple files with custom message and emoji support')
+  .usage('<action>')
   .parse(process.argv)
 
-var gci = require(path.join('../lib'));
-gci();
+var command = program.args[0]
+
+/**
+ * Help.
+ */
+
+program.on('--help', function () {
+ console.log('  Actions :')
+ console.log()
+ console.log('    add     add multiple untracked files in what will be committed')
+ console.log('    commit  commit multiple files with an emoji and a custom message')
+ console.log()
+})
+
+program.parse(process.argv)
+if ((program.args.length != 1) || ( ['add', 'commit'].indexOf(command) == -1 )) return program.help()
+
+var gci = require(path.join('../lib'))
+gci(command)
